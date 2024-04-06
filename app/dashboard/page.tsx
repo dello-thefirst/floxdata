6b6689b7251c7178cd0aadf0b7589fee42568685
Dello <vicctheerapper@gmail.com>
2024-04-06 01:07:07 +0100

    efjwibdljwe

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,7 @@ function Home() {
         <p className="mb-5 hidden">Actions</p>
         <div className="w-full inner flex gap-8 sm:grid sm:gap-4 sm:grid-cols-2 sm:px-1">
           <div className="w-[140px] h-auto rounded-xl bg-[var(--bg-secondary)] p-5 flex flex-col gap-5 sm:w-full">
-            <div className="w-[60px] h-[60px] rounded-full bg-purple-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
+            <div className="w-[60px] h-[60px] sm:w-[50px] sm:h-[50px] rounded-full bg-purple-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
               <i className="fa-regular fa-phone text-purple-500 text-[20px]"></i>
             </div>
             <p className="text-[14px] text-gray-300">Buy Airtime</p>
@@ -17,7 +17,7 @@ function Home() {
           </div>
 
           <div className="w-[140px] h-auto rounded-xl bg-[var(--bg-secondary)] p-5 flex flex-col gap-5 sm:w-full">
-            <div className="w-[60px] h-[60px] rounded-full bg-pink-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
+            <div className="w-[60px] h-[60px] sm:w-[50px] sm:h-[50px] rounded-full bg-pink-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
               <i className="fa-regular fa-wifi text-pink-500 text-[20px]"></i>
             </div>
             <p className="text-[14px] text-gray-300">Buy Data</p>
@@ -25,7 +25,7 @@ function Home() {
           </div>
 
           <div className="w-[140px] h-auto rounded-xl bg-[var(--bg-secondary)] p-5 flex flex-col gap-5 sm:w-full">
-            <div className="w-[60px] h-[60px] rounded-full bg-blue-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
+            <div className="w-[60px] h-[60px] sm:w-[50px] sm:h-[50px] rounded-full bg-blue-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
               <i className="fa-regular fa-lightbulb-on text-blue-500 text-[20px]"></i>
             </div>
             <p className="text-[14px] text-gray-300">Pay Bills</p>
@@ -33,7 +33,7 @@ function Home() {
           </div>
 
           <div className="w-[140px] h-auto rounded-xl bg-[var(--bg-secondary)] p-5 flex flex-col gap-5 sm:w-full">
-            <div className="w-[60px] h-[60px] rounded-full bg-orange-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
+            <div className="w-[60px] h-[60px] sm:w-[50px] sm:h-[50px] rounded-full bg-orange-500 bg-opacity-10 flex gap-3 shadow-xl justify-center items-center cursor-pointer">
               <i className="fa-regular fa-money-bill-transfer text-orange-500 text-[20px]"></i>
             </div>
             <p className="text-[14px] text-gray-300">Convert Airtime</p>
