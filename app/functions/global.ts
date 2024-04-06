ab0c8f5b88e7f3ebb3c981fd8c804dbefd5559bf
Dello <vicctheerapper@gmail.com>
2024-04-06 01:27:20 +0100

    djblccd

--- app/functions/global.ts
+++ app/functions/global.ts
@@ -1,7 +1,7 @@
 export function getTimeOfDay() {
   const currentHour = new Date().getHours();
 
-  if (currentHour >= 5 && currentHour < 12) {
+  if (currentHour >= 0 && currentHour < 12) {
     return "morning";
   } else if (currentHour >= 12 && currentHour < 18) {
     return "afternoon";
