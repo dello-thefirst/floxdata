1988871d56b53b0f40de8cb9aa1862ddd46f0b65
Dello <vicctheerapper@gmail.com>
2024-04-06 12:18:59 +0100

    snavckaj

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { getTimeOfDay } from "../functions/global";
 import { useUserContext } from "../UserData";
 import maleAvatar from "@/app/assets/icons/avatar_male.jpg";
-import unknownAvatar from "@/app/assets/icons/avatar_unknown.jpg";
 import Image from "next/image";
 
 export function AvatarSkeleton() {
@@ -15,7 +14,7 @@ export default function Header() {
   const userDataObject = !isUserDataLoading ? JSON.parse(userData) : "";
 
   return (
-    <header className="w-full h-[70px] flex items-center px-[4%] justify-between sm:px-[5%] sm:h-[90px]">
+    <header className="w-full h-[70px] flex items-center shadow-lg px-[4%] justify-between sm:px-[5%] sm:h-[90px]">
       <div className="welcome-msg flex gap-3">
         {!isUserDataLoading ? (
           <Image
@@ -36,14 +35,14 @@ export default function Header() {
           {isUserDataLoading ? (
             <p className="w-[90px] bg-gray-600 h-2 mt-1 rounded-lg"></p>
           ) : (
-            <p className="username-txt text-[var(--color-2)]">
+            <p className="username-txt text-[var(--color-2)] text-[13px]">
               {isUserDataLoading ? "..." : userDataObject.name}
             </p>
           )}
         </div>
       </div>
 
-      <div className="notification-cont w-[30px] h-[30px] rounded-full bg-[var(--bg-secondary)] flex items-center justify-center text-[var(--color-1)]">
+      <div className="notification-cont w-[30px] h-[30px] shadow-lg rounded-full bg-[var(--bg-secondary)] flex items-center justify-center text-[var(--color-1)]">
         <i className="fa-solid fa-bell"></i>
       </div>
     </header>
