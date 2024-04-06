bfc96638deedbdc0e8f707217391dc7de8bb1744
Dello <vicctheerapper@gmail.com>
2024-04-06 02:32:35 +0100

    hedvlhwhd

--- app/components/Balance.tsx
+++ app/components/Balance.tsx
@@ -4,21 +4,12 @@ import { useUserContext } from "../UserData";
 
 function Balance() {
   const [mainBalance, setMainBalance] = useState(0);
-  const [referralBalance, setReferralBalance] = useState(0);
   //...
   const [isMainBalanceToggled, setIsMainBalanceToggled] = useState(true);
-  const [isReferralBalanceToggled, setIsReferralBalanceToggled] =
-    useState(true);
-  //...
 
-  const toggleBalance = (balanceState: string) => {
-    if (balanceState == "isMainBalanceToggled") {
-      if (isMainBalanceToggled) setIsMainBalanceToggled(false);
-      else setIsMainBalanceToggled(true);
-    } else {
-      if (isReferralBalanceToggled) setIsReferralBalanceToggled(false);
-      else setIsReferralBalanceToggled(true);
-    }
+  const toggleBalance = () => {
+    if (isMainBalanceToggled) setIsMainBalanceToggled(false);
+    else setIsMainBalanceToggled(true);
   };
 
   const { userData, isUserDataLoading } = useUserContext();
@@ -27,18 +18,15 @@ function Balance() {
     setMainBalance(
       !isUserDataLoading && userDataObject.account_balance[0]?.main_balance
     );
-    setReferralBalance(
-      !isUserDataLoading && userDataObject.account_balance[0]?.referral_balance
-    );
   }, [isUserDataLoading]);
   return (
     <div className="sec-1 w-full">
-      <div className="w-[380px] h-[140px] rounded-xl p-5 flex justify-between items-center shadow-2xl relative bg-gradient-to-l from-[#ef14ff] to-[#24266dd8] sm:w-full">
+      <div className="w-[380px] h-[140px] rounded-2xl p-5 flex justify-between items-center shadow-2xl relative bg-gradient-to-l from-[#b514ff] to-[#24266dd8] sm:w-full">
         <div>
           <p className="text-[11px] text-gray-400 font-bold">
             Total Balance: &nbsp;
             <i
-              onClick={() => toggleBalance("isMainBalanceToggled")}
+              onClick={() => toggleBalance()}
               className={`fa-regular ${
                 isMainBalanceToggled ? "fa-eye" : "fa-eye-slash"
               } cursor-pointer font-bold`}
@@ -54,7 +42,7 @@ function Balance() {
           </p>
         </div>
 
-        <div className="w-[50px] h-[50px] rounded-full bg-gray-600 flex justify-center items-center text-gray-200 shadow-xl text-[20px] font-bold">
+        <div className="w-[50px] h-[50px] rounded-xl bg-gray-300 flex justify-center items-center text-gray-700 shadow-xl text-[20px] font-bold cursor-pointer">
           <i className="fa-solid fa-sharp fa-plus"></i>
         </div>
       </div>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,6 +6,10 @@ import maleAvatar from "@/app/assets/icons/avatar_male.jpg";
 import unknownAvatar from "@/app/assets/icons/avatar_unknown.jpg";
 import Image from "next/image";
 
+export function AvatarSkeleton() {
+  return <div className="w-[40px] h-[40px] bg-gray-700 rounded-full"></div>;
+}
+
 export default function Header() {
   const { userData, isUserDataLoading } = useUserContext();
   const userDataObject = !isUserDataLoading ? JSON.parse(userData) : "";
@@ -13,25 +17,29 @@ export default function Header() {
   return (
     <header className="w-full h-[70px] flex items-center px-[4%] justify-between sm:px-[5%] sm:h-[90px]">
       <div className="welcome-msg flex gap-3">
-        <Image
-          className="w-[40px] h-[40px] rounded-full"
-          src={!isUserDataLoading ? maleAvatar : unknownAvatar}
-          alt=""
-          width={40}
-          height={40}
-        ></Image>
+        {!isUserDataLoading ? (
+          <Image
+            className="w-[40px] h-[40px] rounded-full"
+            src={maleAvatar}
+            alt=""
+            width={40}
+            height={40}
+          ></Image>
+        ) : (
+          <AvatarSkeleton />
+        )}
+
         <div>
           <p className="welcome-txt text-[10px] font-normal text-gray-400">
             Good {getTimeOfDay()},
           </p>
-          {userData &&
-            (isUserDataLoading ? (
-              <p className="skeleton h-3 rounded-lg"> </p>
-            ) : (
-              <p className="username-txt text-[14px] text-gray-300">
-                {isUserDataLoading ? "..." : userDataObject.name}
-              </p>
-            ))}
+          {isUserDataLoading ? (
+            <p className="w-[90px] bg-gray-600 h-2 mt-1 rounded-lg"></p>
+          ) : (
+            <p className="username-txt text-[14px] text-gray-300">
+              {isUserDataLoading ? "..." : userDataObject.name}
+            </p>
+          )}
         </div>
       </div>
 
