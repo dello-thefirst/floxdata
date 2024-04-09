5450c35e716319be377b52987bcaebb43cc0d7cf
Dello <vicctheerapper@gmail.com>
2024-04-09 11:43:37 +0100

    dwvkgvegvwh

--- app/components/Balance.tsx
+++ app/components/Balance.tsx
@@ -19,7 +19,7 @@ function Balance() {
   }, [isUserDataLoading]);
   return (
     <div className="sec-1 w-full">
-      <div className="w-[380px] h-[150px] rounded-[1rem] shadow-xl p-5 flex flex-col justify-between relative bg-purple-600/20 sm:w-full">
+      <div className="w-[380px] h-[150px] rounded-[1rem] shadow-sm p-5 flex flex-col justify-between relative bg-purple-600/20 sm:w-full">
         <div>
           <p className="text-[11px] text-gray-600 font-bold uppercase tracking-[1px]">
             Total Balance: &nbsp;
