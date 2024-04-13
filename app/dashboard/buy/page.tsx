ebb42cc4e86115339d90f06ae46400fae20e6e9d
Dello <vicctheerapper@gmail.com>
2024-04-13 15:41:55 +0100

    hvdshvshd

--- app/dashboard/buy/page.tsx
+++ app/dashboard/buy/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Buy() {
-  return <div>Buy</div>;
+  return (
+    <div className="wrapper w-full h-full px-[3%]">
+      <div className="w-full h-auto mb-5">
+        <div className="w-[160px] h-[35px] bg-white rounded-lg drop-shadow-md flex items-center gap-1 cursor-pointer justify-center text-[12px] sm:text-[10px] sm:w-[120px] sm:h-[25px]">
+          Buy Data / Airtime
+          <i className="fa-solid fa-paper-plane"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
