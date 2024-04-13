4694317845c5ced7b1b79447b191a56fedd27aef
Dello <vicctheerapper@gmail.com>
2024-04-13 15:11:27 +0100

    dbwlbijcd

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,32 +4,32 @@ import React from "react";
 export default function Footer({ className }: { className?: string }) {
   return (
     <div
-      className={`w-full fixed h-[40px] bg-gray-50 shadow-2xl bottom-0 flex justify-center items-center px-[3%] ${className}`}
+      className={`w-full fixed h-[50px] bg-gray-50 drop-shadow-2xl shad bottom-0 flex justify-center items-center px-7 ${className}`}
     >
-      <ul className="w-full h-full items-center flex justify-between">
+      <ul className="w-full h-full items-center flex justify-between text-[20px]">
         <li>
           <Link href={"/dashboard"}>
-            <i className="fa-regular fa-house"></i>
+            <i className="fa-light fa-house"></i>
           </Link>
         </li>
         <li>
           <Link href={"/dashboard"}>
-            <i className="fa-regular fa-house"></i>
+            <i className="fa-light fa-paper-plane"></i>
           </Link>
         </li>
         <li>
           <Link href={"/dashboard"}>
-            <i className="fa-regular fa-house"></i>
+            <i className="fa-light fa-shuffle"></i>
           </Link>
         </li>
         <li>
           <Link href={"/dashboard"}>
-            <i className="fa-regular fa-house"></i>
+            <i className="fa-light fa-wallet"></i>
           </Link>
         </li>
         <li>
           <Link href={"/dashboard"}>
-            <i className="fa-regular fa-house"></i>
+            <i className="fa-light fa-user"></i>
           </Link>
         </li>
       </ul>
