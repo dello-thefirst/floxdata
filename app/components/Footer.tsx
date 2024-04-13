734b75d5a93db9772f4ba54dc9dca9e3c075fac7
Dello <vicctheerapper@gmail.com>
2024-04-13 15:29:10 +0100

    bdqiuw\

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,19 +9,19 @@ export default function Footer({ className }: { className?: string }) {
     <div
       className={`w-full fixed h-[50px] bg-white border-[0.5px] shad bottom-0 flex justify-center items-center px-7 ${className}`}
     >
-      <ul className="w-full h-full items-center flex justify-between text-[20px]">
+      <ul className="w-full h-full items-center flex justify-between text-[18px]">
         <li>
           <Link href={"/dashboard"}>
             <i
               className={`fa-thin fa-house-blank ${
-                page == "/dashboard" && "text-purple-600 fa-regular"
+                page == "/dashboard" && "text-purple-600"
               }`}
             ></i>
           </Link>
         </li>
         <li>
           <Link href={"/dashboard"}>
-            <i className={`fa-thin fa-paper-plane ${page == '/dashboard/buy' && 'text-purple-600 fa-regular'}`}></i>
+            <i className={`fa-thin fa-paper-plane ${page == '/dashboard/buy' && 'text-purple-600'}`}></i>
           </Link>
         </li>
         <li>
