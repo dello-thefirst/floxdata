b0f918327c360b39d789a6893911baf91e1621c0
Dello <vicctheerapper@gmail.com>
2024-04-13 17:12:25 +0100

    jwhuwhjndwqs

--- app/dashboard/buy/page.tsx
+++ app/dashboard/buy/page.tsx
@@ -1,10 +1,13 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import airtelLogo from "@/app/assets/logos/airtel.png";
 import mtnLogo from "@/app/assets/logos/mtn.png";
 import gloLogo from "@/app/assets/logos/glo.png";
+import nineMobileLogo from "@/app/assets/logos/9mobile.png";
 
 export default function Buy() {
+  const [mobileNetwork, setMobileNetwork] = useState("airtel")
   return (
     <div className="wrapper w-full h-full px-[3%]">
       <div className="w-full h-auto mb-5">
@@ -14,12 +17,12 @@ export default function Buy() {
         </div>
       </div>
 
-      <div className="w-full rounded-2xl bg-gray-100 h-[100px] p-5">
+      <div className="w-full rounded-2xl bg-gray-100 h-auto p-5">
         <div>
           <div className="dropdown">
             <div tabIndex={0} role="button" className="flex items-center gap-2">
               <Image
-                src={airtelLogo}
+                src={mobileNetwork == "airtel" ? airtelLogo : (mobileNetwork == "mtn" ? mtnLogo : (mobileNetwork == "glo" ? gloLogo : (mobileNetwork == "nineMobile" ?  nineMobileLogo : airtelLogo)))}
                 alt=""
                 className="w-[40px] h-[40px] bg-gray-500 rounded-full"
               ></Image>
@@ -30,7 +33,7 @@ export default function Buy() {
               tabIndex={0}
               className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
+              <li onClick={() => setMobileNetwork("airtel")}>
                 <a>
                   <Image
                     src={airtelLogo}
@@ -40,7 +43,7 @@ export default function Buy() {
                   Airtel
                 </a>
               </li>
-              <li>
+              <li onClick={() => setMobileNetwork("mtn")}>
                 <a>
                   {" "}
                   <Image
@@ -51,8 +54,7 @@ export default function Buy() {
                   MTN
                 </a>
               </li>
-
-              <li>
+              <li onClick={() => setMobileNetwork("glo")}>
                 <a>
                   {" "}
                   <Image
@@ -61,6 +63,17 @@ export default function Buy() {
                     className="w-[30px] h-[30px] rounded-full object-cover"
                   ></Image>
                   GLO
+                </a>
+              </li>
+              <li onClick={() => setMobileNetwork("nineMobile")}>
+                <a>
+                  {" "}
+                  <Image
+                    src={nineMobileLogo}
+                    alt=""
+                    className="w-[30px] h-[30px] rounded-full object-cover"
+                  ></Image>
+                  9mobile
                 </a>
               </li>
             </ul>
